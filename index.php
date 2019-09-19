b0e1236858020b430dc8d6ddf28b0d5525a4a41b
Gold3nDarkness <zlmasters2@gmail.com>
2019-09-19 02:32:50 -0400

    Subiendo informacion.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tarea 1</title>
+</head>
+<body>
+    <?php 
+    $Nombre = "Jose Rafael";
+    $Apellido = "Jorge Guerrero";
+    $Experiencia="Tengo un poco de experiencia en la programacion ya que realice el tencologo en programacion.";
+    $Hobbies ="Mis hobbies son jugar videojuegos, ver anime, dibujar, escuchar musica y ver memes.</br>
+    Supongo que es algo normal hoy en dia.";
+    
+    echo "<h1>Saludos, Soy $Nombre $Apellido</h1>";
+    echo "<p>$Experiencia</p>";
+    echo "<p>$Hobbies</p>";
+    ?>
+</body>
+</html>
